e849e2b8ced9a7e3a55856814330a55a48f68684
timcapps <timcapps@gmail.com>
2020-05-13 10:27:05 -0400

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +8,7 @@ import { ExampleFormComponent } from './example-form/example-form.component';
 import { ScaledTableComponent } from './scaled-table/scaled-table.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule, ReactiveFormsModule ],
   declarations: [ AppComponent, ExampleFormComponent, ScaledTableComponent ],
   bootstrap:    [ AppComponent ]
 })
